8aaa034c90eaa3083f9104f5709e175ede9c4979
tvibhu12 <tvibhu12@gmail.com>
2019-05-12 19:15:19 +0530

    setup project , add Logo, add navBar

---
+++ src/component/Navigator/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navigation=()=>{
+    return(
+        <nav style={{display:'flex',justifyContent:'flex-end'}}>
+    
+    <p className="pa3 f3 dim pointer link">Sing Out</p>
+        </nav>
+    );
+}
+export default Navigation;
