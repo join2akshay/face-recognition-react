fde945f4d4299cd1f71ac88c72203a4fa88384d9
tvibhu12 <tvibhu12@gmail.com>
2019-05-18 22:05:25 +0530

    add api to render the data about image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Logo from './component/Logo/Logo';
 import ImageFrom from './component/ImageFrom/ImageFrom';
 import Rank from './component/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import config from './config';
 import './App.css';
+const app = new Clarifai.App({
+  apiKey: config.MY_KEY
+ });
+ 
 const particleValue={
   particles: {
     number:{
@@ -28,10 +34,19 @@ class App extends Component {
    console.log(event.target.value);
  };
  onButtonSubmit=()=>{
-   console.log('I\'m Clicked');
+  app.models.predict("e0be3b9d6a454f0493ac3a30784001ff", "https://samples.clarifai.com/apparel.jpeg").then(
+    function(response) {
+      // do something with response
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
  };
  render(){
   return (
+    
     <div className="App">
      <Particles className='particle'
                 params={particleValue} />
